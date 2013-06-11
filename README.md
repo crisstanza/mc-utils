d5a2f9ac51f2d1c3fc71705062b2412a0a7906a8
crisstanza <crisstanza@home>
2013-06-11 01:15:52 -0300

    Start and quit game (search for pid)

--- README.md
+++ README.md
@@ -2,7 +2,3 @@ mc-utils
 ========
 
 Utilities to copy, back-up and update zip files.
-
-Hints:
-
-- use "force update" after install Minecraft Forge for the first time.
--- mc-utils.sh
+++ mc-utils.sh
@@ -9,7 +9,7 @@ FONT_YELLOW="\033[33m"
 FONT_BLUE="\033[34m"
 
 AUTO_CLEAN_UP=1
-CLEAR_SCREEN=0
+CLEAR_SCREEN=1
 
 SYSTEM_APPLICATIONS_FOLDER="/Applications"
 
@@ -51,22 +51,22 @@ function doSetup() {
 		fi
 		#
 		#if [ ! -d "$MAIN_FILE_FOLDER/$MAIN_FILE" ] ; then
-		#	doQuit "${FONT_RED}Arquivo '$MAIN_FILE_FOLDER/$MAIN_FILE' não encontrado!${FONT_DEFAULT}";
+		#	doQuit "${FONT_RED}Arquivo '$MAIN_FILE_FOLDER/$MAIN_FILE' não encontrado!${FONT_DEFAULT}"
 		#fi
 		#
 		if [ ! -d "$MINECRAFT_INSTALLER_FOLDER/$MINECRAFT_INSTALLER_FILE" ] ; then
-			doQuit "${FONT_RED}Arquivo '$MINECRAFT_INSTALLER_FOLDER/$MINECRAFT_INSTALLER_FILE' não encontrado!${FONT_DEFAULT}";
+			doQuit "${FONT_RED}Arquivo '$MINECRAFT_INSTALLER_FOLDER/$MINECRAFT_INSTALLER_FILE' não encontrado!${FONT_DEFAULT}"
 		fi
 		#
 		#if [ ! -d "$MINECRAFT_FORGE_INSTALLER_FOLDER/$MINECRAFT_FORGE_INSTALLER_FILE" ] ; then
-		#	doQuit "${FONT_RED}Arquivo '$MINECRAFT_FORGE_INSTALLER_FOLDER/$MINECRAFT_FORGE_INSTALLER_FILE' não encontrado!${FONT_DEFAULT}";
+		#	doQuit "${FONT_RED}Arquivo '$MINECRAFT_FORGE_INSTALLER_FOLDER/$MINECRAFT_FORGE_INSTALLER_FILE' não encontrado!${FONT_DEFAULT}"
 		#fi
 		#
 		#if [ ! -d "$SKINS_FOLDER/$SKIN_CHAR_FILE" ] ; then
-		#	doQuit "${FONT_RED}Arquivo '$SKINS_FOLDER/$SKIN_CHAR_FILE' não encontrado!${FONT_DEFAULT}";
+		#	doQuit "${FONT_RED}Arquivo '$SKINS_FOLDER/$SKIN_CHAR_FILE' não encontrado!${FONT_DEFAULT}"
 		#fi
 	else
-		doQuit "${FONT_RED}Diretório '$THINGS_FOLDER' não encontrado!${FONT_DEFAULT}";
+		doQuit "${FONT_RED}Diretório '$THINGS_FOLDER' não encontrado!${FONT_DEFAULT}"
 	fi
 }
 
@@ -82,12 +82,12 @@ function doQuit() {
 
 function doBackup() {
 	cp "$MAIN_FILE_FOLDER/$MAIN_FILE" "$BKP_FOLDER"
-	showMainMenu "${FONT_BLUE}Back-up efetuado com sucesso!${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Back-up efetuado com sucesso!${FONT_DEFAULT}"
 }
 
 function doBackupRestore() {
 	cp "$BKP_FOLDER/$MAIN_FILE" "$MAIN_FILE_FOLDER"
-	showMainMenu "${FONT_BLUE}Back-up restaurado com sucesso!${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Back-up restaurado com sucesso!${FONT_DEFAULT}"
 }
 
 function doInstallMinecraftForge() {
@@ -98,7 +98,7 @@ function doInstallMinecraftForge() {
 	if [ "$AUTO_CLEAN_UP" == 1 ] ; then
 		rm -R "$TMP_FOLDER/$MINECRAFT_FORGE_INSTALLER_FILE_NAME.unzip"
 	fi
-	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}"
 }
 
 function doInstallPixelmon() {
@@ -109,34 +109,49 @@ function doInstallPixelmon() {
 	if [ "$AUTO_CLEAN_UP" == 1 ] ; then
 		rm -R "$TMP_FOLDER/$PIXELMON_INSTALLER_FILE_NAME.unzip"
 	fi
-	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}"
 }
 
 function doInstallSkinChar() {
 	cd "$SKIN_CHAR_FOLDER"
 	zip -r "$MAIN_FILE_FOLDER/$MAIN_FILE" "$SKIN_CHAR_FILE"
-	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}"
 }
 
 function doUninstallSkinChar() {
 	cd "$SKIN_CHAR_FOLDER_ORIG"
 	zip -r "$MAIN_FILE_FOLDER/$MAIN_FILE" "$SKIN_CHAR_FILE"
-	showMainMenu "${FONT_BLUE}Desinstalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Desinstalação concluída com sucesso! Reinicíe o jogo.${FONT_DEFAULT}"
 }
 
 function doInstall() {
 	cp -R "$MINECRAFT_INSTALLER_FOLDER/$MINECRAFT_INSTALLER_FILE" "$SYSTEM_APPLICATIONS_FOLDER/$MINECRAFT_INSTALLER_FILE"
-	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso!${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Instalação concluída com sucesso!${FONT_DEFAULT}"
 }
 
 function doUninstall() {
 	rm -R "$SYSTEM_APPLICATIONS_FOLDER/$MINECRAFT_FORGE_INSTALLER_FILE"
-	showMainMenu "${FONT_RED}Desinstalação concluída com sucesso!${FONT_DEFAULT}";
+	showMainMenu "${FONT_RED}Desinstalação concluída com sucesso!${FONT_DEFAULT}"
 }
 
 function doCleanUpPersonalFolder() {
 	rm -R "$MAIN_FILE_FOLDER_ROOT"
-	showMainMenu "${FONT_BLUE}Pasta '$MAIN_FILE_FOLDER_ROOT' removida com sucesso!${FONT_DEFAULT}";
+	showMainMenu "${FONT_BLUE}Pasta '$MAIN_FILE_FOLDER_ROOT' removida com sucesso!${FONT_DEFAULT}"
+}
+
+function doStartGame() {
+	open "$SYSTEM_APPLICATIONS_FOLDER/$MINECRAFT_INSTALLER_FILE"
+	showMainMenu "${FONT_BLUE}Jogo iniciado! Verifique a outra janela. ${FONT_DEFAULT}"
+}
+
+function doQuitGame() {
+	local pid=`ps -A | grep -m1 $SYSTEM_APPLICATIONS_FOLDER/[$MINECRAFT_INSTALLER_FILE] | awk '{print $1}'`
+	if [ "$pid" == "" ] ; then
+		showMainMenu "${FONT_RED}O jogo não está rodando!${FONT_DEFAULT}"
+	else
+		kill "$pid"
+		showMainMenu "${FONT_BLUE}Jogo encerrado com sucesso!${FONT_DEFAULT}"
+	fi
 }
 
 function showMainMenu() {
@@ -150,12 +165,14 @@ function showMainMenu() {
 	fi
 	#
 	echo
-	echo "${FONT_GREEN}O que você deseja?${FONT_DEFAULT}";
+	echo "${FONT_GREEN}O que você deseja?${FONT_DEFAULT}"
 	echo
 	echo "   ${FONT_GREEN}1)${FONT_DEFAULT} Instalar Minecraft            ${FONT_GREEN}2)${FONT_DEFAULT} Desinstalar Minecraft      ${FONT_GREEN}3)${FONT_DEFAULT} Apagar pasta pessoal";
 	echo "   ${FONT_GREEN}4)${FONT_DEFAULT} Fazer back-up                 ${FONT_GREEN}5)${FONT_DEFAULT} Restaurar back-up";
 	echo "   ${FONT_GREEN}6)${FONT_DEFAULT} Instalar Skin Personagem      ${FONT_GREEN}7)${FONT_DEFAULT} Desinstalar Skin Personagem";
 	echo "   ${FONT_GREEN}8)${FONT_DEFAULT} Instalar Minecraft Forge      ${FONT_GREEN}9)${FONT_DEFAULT} Instalar Pixelmon";
+	echo
+	echo "   ${FONT_GREEN}R)${FONT_DEFAULT} Rodar o jogo                  ${FONT_GREEN}E)${FONT_DEFAULT} Encerrar o jogo";
 	echo
 	echo "   ${FONT_YELLOW}S)${FONT_DEFAULT} Sair";
 	echo
@@ -181,10 +198,14 @@ function showMainMenu() {
 		doInstallMinecraftForge;
 	elif [ "$option" == "9" ] ; then
 		doInstallPixelmon;
+	elif [ "$option" == "R" -o "$option" == "r" ] ; then
+		doStartGame;
+	elif [ "$option" == "E" -o "$option" == "e" ] ; then
+		doQuitGame;
 	elif [ "$option" == "S" -o "$option" == "s" ] ; then
 		doQuit;
 	else
-		showMainMenu "${FONT_RED}Digite uma das opções válidas!${FONT_DEFAULT}";
+		showMainMenu "${FONT_RED}Digite uma das opções válidas!${FONT_DEFAULT}"
 	fi
 	return;
 }
